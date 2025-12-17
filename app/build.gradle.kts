a13b8a740f5a956852c607441b19240135db9c07
Karan Vishwakarma <karan@Karans-MacBook-Air.local>
2025-12-16 17:12:30 -0800

    elevation changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.noteaiapp.keyboardai"
         minSdk = 24
         targetSdk = 35
-        versionCode = 22
-        versionName = "1.5.1"
+        versionCode = 24
+        versionName = "1.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
